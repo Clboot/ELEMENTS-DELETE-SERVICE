1c52b2cc2d2f8aa80a6043a479dac203218467b8
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-11-05 21:48:58 -0500

    updates

--- server.js
+++ server.js
@@ -1,31 +1,41 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const Controler = require('./server/controler/controler')
-require("dotenv").config()
-const ip = require('ip')
+const express = require("express");
+const bodyParser = require("body-parser");
+const Controler = require("./server/controler/controler");
+require("dotenv").config();
+const ip = require("ip");
 
-require(process.env.EUREKA_HELPER_HOST).registerWithEureka('ELEMENTS-DELETE-SERVICE', process.env.ELEMENTS_DELETE_SERVICE_PORT, ip, process.env.ELEMENTS_DELETE_SERVICE_HOST);
+require(process.env.EUREKA_HELPER_HOST).registerWithEureka(
+  "ELEMENTS-DELETE-SERVICE",
+  process.env.ELEMENTS_DELETE_SERVICE_PORT,
+  ip,
+  process.env.ELEMENTS_DELETE_SERVICE_HOST
+);
 
-
-const app = express()
-app.use(bodyParser.json())
+const app = express();
+app.use(bodyParser.json());
 
 //cors configuration for security
-const appUrl =  `http://${process.env.APP_URL}:${process.env.APP_PORT}`
-const allowedUrls = [appUrl]
+const appUrl = `http://${process.env.APP_URL}:${process.env.APP_PORT}`;
+const allowedUrls = [appUrl];
 app.use(function (req, res, next) {
-  const origin = req.headers.origin
-  res.header('Access-Control-Allow-Credentials', '*');
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'POST');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-    next()
-})
+  const origin = req.headers.origin;
+  res.header("Access-Control-Allow-Credentials", "*");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
+  );
+  next();
+});
 
-app.delete('/recycle_bin', Controler.recycle)
+app.delete("/recycle_bin", Controler.recycle);
 
 app.listen(process.env.ELEMENTS_DELETE_SERVICE_PORT, function () {
-    console.log('Authentication Service listening on: http://localhost:%s', process.env.ELEMENTS_DELETE_SERVICE_PORT)
-})
+  console.log(
+    "Authentication Service listening on: http://localhost:%s",
+    process.env.ELEMENTS_DELETE_SERVICE_PORT
+  );
+});
