52c4084621ba1a983a712b48892b5ce62c83610c
Michel ADJOGNON <kadjogno@isi-mtl.com>
2020-11-06 20:29:17 -0500

    updates

--- server/controler/controler.js
+++ server/controler/controler.js
@@ -1,68 +1,64 @@
 //-----------------------------------All required modules-----------------------------------------
-const manager = require('../manager/manager')
-const remove_offensives = require('./remove_offensives')
-const dao = require('../service/dao')
-require("dotenv").config()
+const manager = require("../manager/manager");
+const remove_offensives = require("./remove_offensives");
+const dao = require("../service/dao");
+require("dotenv").config();
 //-----------------------------------------------------------------------------------------------
 
 //-------------------------------------Create--------------------------------------------------------------//
 
 const recycle = (req, res) => {
+  let reponseJSON = { erreur: process.env.UNKNOWN_ERROR };
 
-    let reponseJSON = { erreur: process.env.UNKNOWN_ERROR }
+  //check if our req.body contains all properties we are expecting
 
-    //check if our req.body contains all properties we are expecting
-    const properties = ['name', 'item_id', 'type', 'id_before', 'parent_id', 'parent']
-    const bodyJsonValue = req.body
-    try {
+  const properties = ["name", "item_id", "type", "parent_id", "parent"];
+  const bodyJsonValue = req.body;
+  try {
+    let newItemsObject = bodyJsonValue.map((item) => {
+      if (
+        !Object.prototype.hasOwnProperty.call(item, "name") ||
+        !Object.prototype.hasOwnProperty.call(item, "item_id") ||
+        !Object.prototype.hasOwnProperty.call(item, "type") ||
+        !Object.prototype.hasOwnProperty.call(item, "parent_id")
+      ) {
+        reponseJSON = { erreur: process.env.PROPERTY_NOT_FOUND };
+        throw new Error("Property not found");
+      }
 
-        if (!Object.prototype.hasOwnProperty.call(bodyJsonValue, 'items')) {
-            reponseJSON = { erreur: process.env.PROPERTY_NOT_FOUND }
-            throw new Error('Property not found')
-        }
+      return {
+        name: item.name.trim(),
+        item_id: item.item_id,
+        type: item.type.trim(),
+        parent_id: item.parent_id,
+        parent: item.parent,
+      };
+    });
 
-     
-        let newItemsObject = bodyJsonValue.items.map(item => {
-            if (!Object.prototype.hasOwnProperty.call(item, 'name') || !Object.prototype.hasOwnProperty.call(item, 'item_id') || !Object.prototype.hasOwnProperty.call(item, 'type') || !Object.prototype.hasOwnProperty.call(item, 'id_before') || !Object.prototype.hasOwnProperty.call(item, 'parent_id') || !Object.prototype.hasOwnProperty.call(item, 'parent')) {
-                reponseJSON = { erreur: process.env.PROPERTY_NOT_FOUND }
-                throw new Error('Property not found')
-            }
-
-            return {
-                name: item.name.trim(),
-                item_id : item.item_id,
-                type: item.type.trim(),
-                id_before: item.id_before,
-                parent_id: item.parent_id,
-                parent: item.parent
-            }
-        })
-
-        
-            manager.DeleteProject.delete(newItemsObject).then(result =>{
-                if(result.operation == "200"){
-                    manager.DeleteProject.recycle(newItemsObject).then(result =>{
-                        if(result.operation == "200"){
-                            sendData(res, result)
-                        }
-                })
-
-            }
-                          
-            })
-
-    } catch (err) {      
-        sendData(res, reponseJSON)
-    }
-}
+    manager.DeleteProject.delete(newItemsObject).then((result) => {
+      if (result.operation == "200") {
+        manager.DeleteProject.recycle(newItemsObject).then((result) => {
+          if (result.operation == "200") {
+            sendData(res, result);
+          }
+        });
+      } else {
+        pgJsonResult = { error: process.env.PROJECT_ALREADY_EXISTS };
+        sendData(res, pgJsonResult);
+      }
+    });
+  } catch (err) {
+    sendData(res, reponseJSON);
+  }
+};
 
 //-----------------------------------Send Data Request to Client-------------------------------------------------//
 
 const sendData = (res, searchJsonData) => {
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify(searchJsonData, null, 4))
-}
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(JSON.stringify(searchJsonData, null, 4));
+};
 
 module.exports = {
-    recycle
-}
+  recycle,
+};
--- server/manager/manager.js
+++ server/manager/manager.js
@@ -1,60 +1,61 @@
-const dao = require('../service/dao')
-require("dotenv").config()
+const dao = require("../service/dao");
+require("dotenv").config();
 
 class DeleteProject {
-    static delete (newItemsObject) {
-        let pgJsonResult = null
-        const itemObjectArrayLength = newItemsObject.length
-        return new Promise(resolve => {
-            dao.connect()
-            newItemsObject.map((item, index) =>{
+  static delete(newItemsObject) {
+    let pgJsonResult = null;
+    const itemObjectArrayLength = newItemsObject.length;
+    return new Promise((resolve) => {
+      dao.connect();
+      newItemsObject.map((item, index) => {
+        dao.query(
+          "delete from elements where id = $1",
+          [item.item_id],
+          (result) => {
+            if (result.rowCount > 0) {
+              if (itemObjectArrayLength == index + 1) {
+                pgJsonResult = { operation: 200 };
+                resolve(pgJsonResult);
+                dao.disconnect();
+              }
+            } else {
+              pgJsonResult = { error: process.env.PROJECT_ALREADY_EXISTS };
+              resolve(pgJsonResult);
+              dao.disconnect();
+            }
+          }
+        );
+      });
+    });
+  }
 
-                dao.query('delete from elements where id = $1', [item.item_id], (result) => {
-                    if (result.rowCount > 0) {
-                        if(itemObjectArrayLength == (index + 1)){
-                            pgJsonResult = { operation: 200 }
-                            resolve(pgJsonResult)
-                            dao.disconnect()
-                        }
-                    } else {
-                        pgJsonResult = { error: process.env.PROJECT_ALREADY_EXISTS }
-                        resolve(pgJsonResult)
-                        dao.disconnect()
-                    }
-                  
-                })
-
-            })
-        })
-    }
-
-    static recycle (newItemsObject) {
-        let pgJsonResult = null
-        const itemObjectArrayLength = newItemsObject.length
-        return new Promise(resolve => {
-            dao.connect()
-            newItemsObject.map((item, index) =>{
-
-                dao.query('insert into recycle_bin(name, item_id, type, id_before, parent_id, parent, date_creation) values($1, $2, $3, $4, $5, $6, NOW())', [item.name, item.item_id, item.type, item.id_before, item.parent_id, item.parent], (result) => {
-                    if (result.rowCount > 0) {
-                        if(itemObjectArrayLength === (index + 1)){
-                            pgJsonResult = { operation: 200 }
-                            resolve(pgJsonResult)
-                            dao.disconnect()
-                        }
-                    } else {
-                        pgJsonResult = { error: process.env.PROJECT_ALREADY_EXISTS }
-                        resolve(pgJsonResult)
-                        dao.disconnect()
-                    }
-                  
-                })
-
-            })
-        })
-    }
-
+  static recycle(newItemsObject) {
+    let pgJsonResult = null;
+    const itemObjectArrayLength = newItemsObject.length;
+    return new Promise((resolve) => {
+      dao.connect();
+      newItemsObject.map((item, index) => {
+        dao.query(
+          "insert into recycle_bin(name, item_id, type, parent_id, parent, date_creation) values($1, $2, $3, $4, $5, NOW())",
+          [item.name, item.item_id, item.type, item.parent_id, item.parent],
+          (result) => {
+            if (result.rowCount > 0) {
+              if (itemObjectArrayLength === index + 1) {
+                pgJsonResult = { operation: 200 };
+                resolve(pgJsonResult);
+                dao.disconnect();
+              }
+            } else {
+              pgJsonResult = { error: process.env.PROJECT_ALREADY_EXISTS };
+              resolve(pgJsonResult);
+              dao.disconnect();
+            }
+          }
+        );
+      });
+    });
+  }
 }
 module.exports = {
-    DeleteProject
-}
+  DeleteProject,
+};
